c105f264966789999a567b9ab99dded64a6c3067
PaolaP <paolaperez8@gmail.com>
2022-04-20 01:05:51 -0400

    fixed the instances names

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// http server code
 const http = require("http");
-const { connection } = require("websocket");
+//const { connection } = require("websocket");
+//websocket which allows for the handshake
 const WebSocketServer = require("websocket").server;
 let connection = null;
 
@@ -11,11 +13,12 @@ const websocket = new WebSocketServer({
   "httpServer": httpserver
 })
 
+//onaccept send back the switching protocol and get a connection as a result.
 websocket.on ("request", request => {
   connection = request.accept(null, request.origin)
-  connection.on("onopen", () => console.log("opened!"))
-  connection.on("onclose", () => console.log("closed!"))
-  connection.on("onmessage", message => console.log(`Received message ${message}`))
+  connection.on("open", () => console.log("opened!"))
+  connection.on("close", () => console.log("closed!"))
+  connection.on("message", message => console.log(`Received message ${message.utf8Data}`))
 })
 
 httpserver.listen(8080, () => console.log("My server is listening"))
